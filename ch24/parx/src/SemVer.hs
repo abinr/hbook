b57848b96f8e10336580bc81dfd67d89ba77775c
@abinr <brianfarrar@icloud.com>
2018-06-23 09:07:17 +0200

    added Ordering instance

--- ch24/parx/src/SemVer.hs
+++ ch24/parx/src/SemVer.hs
@@ -10,6 +10,29 @@ data SemVer
   = SemVer Major Minor Patch Release Metadata
   deriving (Eq, Show)
 
+-- Tests pass so far but this implementation seems overcomplex
+instance Ord SemVer where
+  compare (SemVer major minor patch rel _) (SemVer major' minor' patch' rel' _) =
+    let
+      a = listToInteger . fmap toInteger $ [major, minor, patch]
+      b = listToInteger . fmap toInteger $ [major', minor', patch']
+    in
+      case a `compare` b of
+        GT -> GT
+        LT -> LT
+        EQ ->
+          case (rel, rel') of
+            ([], []) -> EQ
+            ([], _) -> GT
+            (_, []) -> LT
+            (_, _) ->
+              let
+                x = fmap (uncurry compare) $ zip rel rel'
+                y = compare (length rel) (length rel')
+              in
+                mconcat x <> y
+
+
 type Major = Natural
 type Minor = Natural
 type Patch = Natural
@@ -20,6 +43,13 @@ data NumberOrString
   = NOSS String
   | NOSI Natural
   deriving (Eq, Show)
+
+instance Ord NumberOrString where
+  compare (NOSS s) (NOSS s') = compare s s'
+  compare (NOSI n) (NOSI n') = compare n n'
+  compare (NOSS s) (NOSI n)  = GT
+  compare (NOSI n) (NOSS s)  = LT
+
 
 parseSemVer :: Parser SemVer
 parseSemVer = do
--- ch24/parx/test/SemVerSpec.hs
+++ ch24/parx/test/SemVerSpec.hs
@@ -2,6 +2,7 @@ module SemVerSpec where
 
 import Test.Hspec
 import qualified Text.Trifecta as P
+import Data.List (sort)
 import SemVer
 
 main :: IO ()
@@ -11,33 +12,61 @@ spec :: Spec
 spec = do
   describe "SemVer Parser" $ do
     it "should parse 2.1.1" $ do
-      let m = P.parseString parseSemVer mempty "2.1.1"
-      maybeResult m `shouldBe` Just (SemVer 2 1 1 [] [])
+      let m = parx "2.1.1"
+      m `shouldBe` Just (SemVer 2 1 1 [] [])
     it "should parse 1.0.0-x.7.z.92" $ do
-      let m = P.parseString parseSemVer mempty "1.0.0-x.7.z.92"
+      let m = parx "1.0.0-x.7.z.92"
           e = SemVer 1 0 0 [NOSS "x", NOSI 7, NOSS "z", NOSI 92] []
-      maybeResult m `shouldBe` Just e
+      m `shouldBe` Just e
     it "should parse 1.0.0-alpha" $ do
-      let m = P.parseString parseSemVer mempty "1.0.0-alpha"
-      maybeResult m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha"] [])
+      let m = parx "1.0.0-alpha"
+      m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha"] [])
     it "should parse 1.0.0-alpha.1" $ do
-      let m = P.parseString parseSemVer mempty "1.0.0-alpha.1"
-      maybeResult m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha", NOSI 1] [])
+      let m = parx "1.0.0-alpha.1"
+      m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha", NOSI 1] [])
     it "should parse 1.0.0-0.3.7" $ do
-      let m = P.parseString parseSemVer mempty "1.0.0-0.3.7"
-      maybeResult m `shouldBe` Just (SemVer 1 0 0 [NOSI 0, NOSI 3, NOSI 7] [])
+      let m = parx "1.0.0-0.3.7"
+      m `shouldBe` Just (SemVer 1 0 0 [NOSI 0, NOSI 3, NOSI 7] [])
     it "should parse 1.0.0-alpha+001" $ do
-      let m = P.parseString parseSemVer mempty "1.0.0-alpha+001"
-      maybeResult m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha"] ["001"])
+      let m = parx "1.0.0-alpha+001"
+      m `shouldBe` Just (SemVer 1 0 0 [NOSS "alpha"] ["001"])
     it "should parse 0.0.1-prealpha.0.31.2+001.wvs" $ do
-      let m = P.parseString parseSemVer mempty "0.0.1-pre-alpha.0.31.2+001.w--5vs"
+      let m = parx "0.0.1-pre-alpha.0.31.2+001.w--5vs"
           e = SemVer 0 0 1
                 [NOSS "pre-alpha", NOSI 0, NOSI 31, NOSI 2]
                 ["001", "w--5vs"]
-      maybeResult m `shouldBe` Just e
+      m `shouldBe` Just e
     it "should fail leading zeros, 001.0.0" $ do
-      let m = P.parseString parseSemVer mempty "001.0.0"
-      maybeResult m `shouldBe` Nothing
+      let m = parx "001.0.0"
+      m `shouldBe` Nothing
+    it "should respect major version precedence" $ do
+      let lt = parx "1.0.0"
+          gt = parx "2.0.0"
+      lt `compare` gt `shouldBe` LT
+    it "should respect minor version precedence" $ do
+      parx "2.0.0" `compare` parx "2.1.0" `shouldBe` LT
+    it "should respect patch version precedence" $ do
+      parx "2.1.0" `compare` parx "2.1.1" `shouldBe` LT
+    it "should assign precedence to a pre-release " $ do
+      parx "1.0.0-alpha" `compare` parx "1.0.0" `shouldBe` LT
+    it "should assign precedence to pre-release with smaller set" $ do
+      parx "1.0.0-alpha" `compare` parx "1.0.0.-alpha.1" `shouldBe` LT
+      sort expected `shouldBe` expected
+
+expected = fmap parx
+  ["1.0.0-alpha"
+  ,"1.0.0-alpha.1"
+  ,"1.0.0-alpha.beta"
+  ,"1.0.0-beta"
+  ,"1.0.0-beta.2"
+  ,"1.0.0-beta.11"
+  ,"1.0.0-rc.1"
+  ,"1.0.0"
+  ]
+
+
+parx :: String -> Maybe SemVer
+parx = maybeResult . P.parseString parseSemVer mempty
 
 
 maybeResult :: P.Result a -> Maybe a
