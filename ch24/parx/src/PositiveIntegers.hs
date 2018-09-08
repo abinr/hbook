f48f814e4656b4f0457ea0e206aca53165f64d09
abinr <brianfarrar@gmail.com>
2018-09-08 21:52:01 +0200

    ch24 ex 4

--- ch24/parx/src/PositiveIntegers.hs
+++ ch24/parx/src/PositiveIntegers.hs
@@ -1,17 +1,27 @@
 module PositiveIntegers where
 
 import Text.Trifecta
+import Control.Applicative
 import qualified Data.Char as Char
-
-parseDigit :: Parser Char
-parseDigit = satisfyRange '0' '9' <?> "parseDigit"
+import Data.Function
 
 base10Integer :: Parser Integer
 base10Integer =
-  fmap listToInteger $ (fmap . fmap) (toInteger . Char.digitToInt) ints
+  fmap digitsToInteger (some parseDigit)
 
-ints :: Parser String
-ints =  pure (:) <*> parseDigit <*> many parseDigit
+base10Integer' :: Parser Integer
+base10Integer' = do
+  sign <- optional $ char '-'
+  num <- base10Integer
+  case sign of
+    Just '-' -> return $ negate num
+    Nothing -> return num
+
+digitsToInteger :: String -> Integer
+digitsToInteger = listToInteger . fmap (toInteger . Char.digitToInt)
+
+parseDigit :: Parser Char
+parseDigit = satisfyRange '0' '9' <?> "parseDigit"
 
 listToInteger :: [Integer] -> Integer
 listToInteger xs = foldl (\acc a -> acc * 10 + a) 0 xs
